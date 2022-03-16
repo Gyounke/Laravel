3ba2de1b8cb51e9538022ee4ce2ac72a2176ff14
Kevin <blisteinkevin@outlook.com>
2022-03-16 11:30:50 +0100

    laravel-portfolio_start

---
+++ laravel-portfolio/resources/views/welcome.blade.php
@@ -0,0 +1,12 @@
+@extends('layouts/app')
+    @section('content')
+        @include('partials/banner')
+        @include('partials/hero')
+        @include('partials/about')
+        @include('partials/facts')
+        @include('partials/skills')
+        @include('partials/portfolio')
+        @include('partials/testimonial')
+        @include('partials/contact')
+        @include('partials/footer')
+    @endsection
