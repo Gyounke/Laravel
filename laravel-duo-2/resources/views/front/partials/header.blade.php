5d35e7cc659b8c6c63d2fb7fe5d2c5748f7734f7
Kevin <blisteinkevin@outlook.com>
2022-04-07 11:43:40 +0200

    migrations-models-controllers

---
+++ laravel-duo-2/resources/views/front/partials/header.blade.php
@@ -0,0 +1,31 @@
+<!-- ***** Header Area Start ***** -->
+<header class="header-area header-sticky">
+    <div class="container">
+        <div class="row">
+            <div class="col-12">
+                <nav class="main-nav">
+                    <!-- ***** Logo Start ***** -->
+                    <a href="index.html" class="logo">
+                        <img src="{{ asset("images/templatemo-eduwell.png") }}" alt="EduWell Template">
+                    </a>
+                    <!-- ***** Logo End ***** -->
+                    <!-- ***** Menu Start ***** -->
+                    <ul class="nav">
+                        <li class="scroll-to-section"><a href="#top" class="active">Home</a></li>
+                        <li class="scroll-to-section"><a href="#services">Services</a></li>
+                        <li class="scroll-to-section"><a href="#courses">Courses</a></li>
+                        <li class="scroll-to-section"><a href="#testimonials">Testimonials</a></li> 
+                        <li class="scroll-to-section"><a href="#contact-section">Contact Us</a></li>                        
+                        <li class="scroll-to-section"><a href="#contact-section">Login</a></li>
+                        <li class="scroll-to-section"><a href="#contact-section">Register</a></li> 
+                    </ul>        
+                    <a class='menu-trigger'>
+                        <span>Menu</span>
+                    </a>
+                    <!-- ***** Menu End ***** -->
+                </nav>
+            </div>
+        </div>
+    </div>
+</header>
+<!-- ***** Header Area End ***** -->
