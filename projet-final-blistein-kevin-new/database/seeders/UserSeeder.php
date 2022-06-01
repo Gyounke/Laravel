017219f19d3692a85c15ddeced4f4dd61d1919bc
Kevin <blisteinkevin@outlook.com>
2022-06-01 09:27:48 +0200

    seeders-users-KB

--- projet-final-blistein-kevin-new/database/seeders/UserSeeder.php
+++ projet-final-blistein-kevin-new/database/seeders/UserSeeder.php
@@ -25,5 +25,65 @@ class UserSeeder extends Seeder
             'password' => Hash::make('123456'),
             'status' => 1,
         ]);
+
+        DB::table('users')->insert([
+            'lastname' => "Bobi",
+            'firstname' => "Dante",
+            'age' => "38",
+            'address' => "Sake street n°7",
+            'email' => "filou@hotmail.com", 
+            'password' => Hash::make('123456'),
+            'status' => 2,
+        ]);
+
+        DB::table('users')->insert([
+            'lastname' => "Sarah",
+            'firstname' => "Millenium",
+            'age' => "35",
+            'address' => "Op street n°7",
+            'email' => "bandito@hotmail.com", 
+            'password' => Hash::make('123456'),
+            'status' => 2,
+        ]);
+
+        DB::table('users')->insert([
+            'lastname' => "Killian",
+            'firstname' => "Poporov",
+            'age' => "49",
+            'address' => "Padding street n°7",
+            'email' => "tagada@hotmail.com", 
+            'password' => Hash::make('123456'),
+            'status' => 3,
+        ]);
+
+        DB::table('users')->insert([
+            'lastname' => "Zozo",
+            'firstname' => "Sacrieur",
+            'age' => "32",
+            'address' => "ZAD street n°7",
+            'email' => "sacripan@hotmail.com", 
+            'password' => Hash::make('123456'),
+            'status' => 3,
+        ]);
+
+        DB::table('users')->insert([
+            'lastname' => "Mikasa",
+            'firstname' => "Nextgen",
+            'age' => "26",
+            'address' => "Shiganshina street n°7",
+            'email' => "badouss@hotmail.com", 
+            'password' => Hash::make('123456'),
+            'status' => 4,
+        ]);
+
+        DB::table('users')->insert([
+            'lastname' => "Tim",
+            'firstname' => "lafrappe",
+            'age' => "26",
+            'address' => "Thug street n°7",
+            'email' => "lascar@hotmail.com", 
+            'password' => Hash::make('123456'),
+            'status' => 4,
+        ]);
     }
 }
